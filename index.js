a4bfdd6e65ab4fbbd22105d7a2aec640349271a1
b0ndurant <martin.aurelien01@gmail.com>
2019-04-29 00:15:41 +0200

    create methods add, deleteTodo, deleteAll and toggle in index.js

--- index.js
+++ index.js
@@ -7,4 +7,29 @@ new Vue ({
     newQuantity: '',
     filter: 'all',
   },
+  methods: {
+    add: function() {
+      if (this.newName) {
+        (this.newQuantity === '') ? this.newQuantity = 1 : this.newQuantity;
+        this.todos.push({
+          name: this.newName,
+          quantity: this.newQuantity,
+          completed: false,
+        });
+        this.newQuantity = this.newName = this.error = '';
+      }
+      else {
+        this.error = 'Il vous manque votre article !'
+      }
+    },
+    deleteTodo : function(todo) {
+      this.todos.splice(this.todos.indexOf(todo),1)
+    },
+    deleteAll : function() {
+      this.todos = [];
+    },
+    toggle: function(todo) {
+      todo.completed = !todo.completed;
+    },
+  }
 })
